b06ebe8892191a70d3ac6d24ef0b22484f6af01b
PointlessVenture (Aiden Tracy) <aidenrtracy@icloud.com>
2026-06-21 12:42:02 -0400

    Science?

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1056,3 +1056,9 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/TestMap1/scripts.inc"
+
+	.include "data/maps/TestMap01/scripts.inc"
+
+	.include "data/maps/Route01/scripts.inc"
--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -63,6 +63,9 @@
 #define MAP_UNDERWATER_ROUTE129 (54 | (0 << 8))
 #define MAP_UNDERWATER_ROUTE105 (55 | (0 << 8))
 #define MAP_UNDERWATER_ROUTE125 (56 | (0 << 8))
+#define MAP_TEST_MAP1           (57 | (0 << 8))
+#define MAP_TEST_MAP01          (58 | (0 << 8))
+#define MAP_ROUTE01             (59 | (0 << 8))
 
 // gMapGroup_IndoorLittleroot
 #define MAP_LITTLEROOT_TOWN_BRENDANS_HOUSE_1F    (0 | (1 << 8))
